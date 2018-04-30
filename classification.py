59f03ea519ad9aeaeb9e18797f8edcfdd7335be0
you871956352 <you871956352@gmail.com>
2018-04-30 21:38:06 +0800

    1. Complete the classification process. 2. Add the report.

--- classification.py
+++ classification.py
@@ -4,35 +4,134 @@ import time
 from sklearn import linear_model
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
-from sklearn.naive_bayes import BernoulliNB
-from sklearn.naive_bayes import GaussianNB
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
 from sklearn import svm
+import warnings
 
 def lr_model(training_df, testing_df):
     start_time = time.time()
+    #Data set split.
+    #Data for training.
     X_train = training_df[['horse_index','jockey_index','trainer_index','draw','actual_weight','race_distance']].values
     y_train = training_df[['finishing_position']].values
+    #Data for testing.
     X_test = testing_df[['horse_index','jockey_index','trainer_index','draw','actual_weight','race_distance']].values
-    y_test = testing_df[['finishing_position']].values
-    
+    y_test = testing_df[['race_id', 'horse_id', 'horse_index','jockey_ave_rank', 'trainer_ave_rank','finishing_position']].values
+    #Model training and predict.
     lr_model = linear_model.LogisticRegression()
     lr_model.fit(X_train, y_train[:,0])
     test_predict = lr_model.predict(X_test)
-    
+    #Label and foramt change.
     result = testing_df[['race_id', 'horse_id', 'horse_index', 'jockey_ave_rank', 'trainer_ave_rank']].values
     result = np.append(result, test_predict[:,None], axis = 1)
     result_df = pd.DataFrame(data=result,columns=['race_id', 'horse_id', 'horse_index','jockey_ave_rank', 'trainer_ave_rank', 'finishing_position'])
     result_df = reorder(result_df)
     result_df = add_All(result_df)
-    result_df.to_csv(path_or_buf = 'predictions/lr_predictions.csv')
-    
+    result_df.to_csv(path_or_buf = 'predictions/lr_predictions.csv')  
+    #Evaluation.
+    truth_df = pd.DataFrame(data=y_test,columns=['race_id', 'horse_id', 'horse_index','jockey_ave_rank', 'trainer_ave_rank', 'finishing_position'])
+    truth_df = add_All(truth_df)
+    print("lr_model: ")
+    prediction_Evaluation(result_df, truth_df)
+    #Running time.
     running_time = time.time() - start_time
-    return result_df,running_time
+    print("Running time : %s seconds" % running_time)
+    return result_df,truth_df
+
+def nb_model(training_df, testing_df):
+    start_time = time.time()
+    #Data set split.
+    #Data for training.
+    X_train = training_df[['horse_index','jockey_index','trainer_index']].values
+    y_train = training_df[['finishing_position']].values
+    #Data for testing.
+    X_test = testing_df[['horse_index','jockey_index','trainer_index']].values
+    y_test = testing_df[['race_id', 'horse_id', 'horse_index','jockey_ave_rank', 'trainer_ave_rank','finishing_position']].values
+    #Model training and predict.
+    nb_model = MultinomialNB()
+    nb_model.fit(X_train, y_train[:,0])
+    test_predict = nb_model.predict(X_test)
+    #Label and foramt change.
+    result = testing_df[['race_id', 'horse_id', 'horse_index', 'jockey_ave_rank', 'trainer_ave_rank']].values
+    result = np.append(result, test_predict[:,None], axis = 1)
+    result_df = pd.DataFrame(data=result,columns=['race_id', 'horse_id', 'horse_index','jockey_ave_rank', 'trainer_ave_rank', 'finishing_position'])
+    result_df = reorder(result_df)
+    result_df = add_All(result_df)
+    result_df.to_csv(path_or_buf = 'predictions/nb_predictions.csv')  
+    #Evaluation.
+    truth_df = pd.DataFrame(data=y_test,columns=['race_id', 'horse_id', 'horse_index','jockey_ave_rank', 'trainer_ave_rank', 'finishing_position'])
+    truth_df = add_All(truth_df)
+    print("nb_model: ")
+    prediction_Evaluation(result_df, truth_df)
+    #Running time.
+    running_time = time.time() - start_time
+    print("Running time : %s seconds" % running_time)
+    return result_df, truth_df
+
+def svm_model(training_df, testing_df):
+    start_time = time.time()
+    #Data set split.
+    #Data for training.
+    X_train = training_df[['recent_ave_rank','jockey_ave_rank','trainer_ave_rank']].values
+    y_train = training_df[['finishing_position']].values
+    #Data for testing.
+    X_test = testing_df[['recent_ave_rank','jockey_ave_rank','trainer_ave_rank']].values
+    y_test = testing_df[['race_id', 'horse_id', 'horse_index','jockey_ave_rank', 'trainer_ave_rank','finishing_position']].values
+    #Model training and predict.
+    mi = 1500
+    svm_model = svm.SVC(kernel='rbf', max_iter=mi)
+    svm_model.fit(X_train, y_train[:,0])
+    test_predict = svm_model.predict(X_test)
+    #Label and foramt change.
+    result = testing_df[['race_id', 'horse_id', 'horse_index', 'jockey_ave_rank', 'trainer_ave_rank']].values
+    result = np.append(result, test_predict[:,None], axis = 1)
+    result_df = pd.DataFrame(data=result,columns=['race_id', 'horse_id', 'horse_index','jockey_ave_rank', 'trainer_ave_rank', 'finishing_position'])
+    result_df = reorder(result_df)
+    result_df = add_All(result_df)
+    result_df.to_csv(path_or_buf = 'predictions/svm_predictions.csv')  
+    #Evaluation.
+    truth_df = pd.DataFrame(data=y_test,columns=['race_id', 'horse_id', 'horse_index','jockey_ave_rank', 'trainer_ave_rank', 'finishing_position'])
+    truth_df = add_All(truth_df)
+    print("svm_model: ")
+    print("Max iteration: ", mi)
+    prediction_Evaluation(result_df, truth_df)
+    #Running time.
+    running_time = time.time() - start_time
+    print("Running time : %s seconds" % running_time)
+    return result_df, truth_df
+
+def rf_model(training_df, testing_df):
+    start_time = time.time()
+    #Data set split.
+    #Data for training.
+    X_train = training_df[['recent_ave_rank','jockey_ave_rank','trainer_ave_rank']].values
+    y_train = training_df[['finishing_position']].values
+    #Data for testing.
+    X_test = testing_df[['recent_ave_rank','jockey_ave_rank','trainer_ave_rank']].values
+    y_test = testing_df[['race_id', 'horse_id', 'horse_index','jockey_ave_rank', 'trainer_ave_rank','finishing_position']].values
+    #Model training and predict.
+    md = 5
+    rf_model = RandomForestClassifier(max_depth=md, random_state=0)
+    rf_model.fit(X_train, y_train[:,0])
+    test_predict = rf_model.predict(X_test)
+    #Label and foramt change.
+    result = testing_df[['race_id', 'horse_id', 'horse_index', 'jockey_ave_rank', 'trainer_ave_rank']].values
+    result = np.append(result, test_predict[:,None], axis = 1)
+    result_df = pd.DataFrame(data=result,columns=['race_id', 'horse_id', 'horse_index','jockey_ave_rank', 'trainer_ave_rank', 'finishing_position'])
+    result_df = reorder(result_df)
+    result_df = add_All(result_df)
+    result_df.to_csv(path_or_buf = 'predictions/rf_predictions.csv')  
+    #Evaluation.
+    truth_df = pd.DataFrame(data=y_test,columns=['race_id', 'horse_id', 'horse_index','jockey_ave_rank', 'trainer_ave_rank', 'finishing_position'])
+    truth_df = add_All(truth_df)
+    print("rf_model: ")
+    print("Max depth: ", md)
+    prediction_Evaluation(result_df, truth_df)
+    #Running time.
+    running_time = time.time() - start_time
+    print("Running time : %s seconds" % running_time)
+    return result_df,truth_df
 
 def convert_to_float_time(frac_str):
     if frac_str.isdigit():
@@ -51,6 +150,7 @@ def reorder(df):
         df_sub = tie_Break(df_sub)
         frames = [df_all, df_sub]
         df_all = pd.concat(frames)
+    df_all = df_all.sort_index()
     return df_all[['race_id', 'horse_id','horse_index','finishing_position']]
 
 def tie_Break(df):
@@ -59,7 +159,7 @@ def tie_Break(df):
     finishingPosition = df[['finishing_position']].drop_duplicates(subset='finishing_position',keep='last')
     for i in range(0, finishingPosition.values.shape[0]):
         df_sub = df[df['finishing_position'].isin(finishingPosition.values[i])]
-        df_sub = df_sub.sort_values(by=['ave_rank'], ascending=False)
+        df_sub = df_sub.sort_values(by=['ave_rank'])
         frames = [df_all, df_sub]
         df_all = pd.concat(frames)
     df_all['finishing_position'] = [x for x in range(1, df_all.values.shape[0] + 1)]
@@ -98,6 +198,24 @@ def add_Label(df, label):
             return '0'
     return -1
 
+def prediction_Evaluation(result, truth):
+    p_score = precision_score(np.asfarray(truth[['HorseWin']].values,float), np.asfarray(result[['HorseWin']].values,float))
+    r_score = recall_score(np.asfarray(truth[['HorseWin']].values,float), np.asfarray(result[['HorseWin']].values,float))
+    print("For Winner: ")
+    print("p_score: ", p_score)
+    print("r_score: ", r_score)
+    p_score = precision_score(np.asfarray(truth[['HorseRankTop3']].values, float), np.asfarray(result[['HorseRankTop3']].values, float))
+    r_score = recall_score(np.asfarray(truth[['HorseRankTop3']].values, float), np.asfarray(result[['HorseRankTop3']].values, float))
+    print("For Top3: ")
+    print("p_score: ", p_score)
+    print("r_score: ", r_score)
+    p_score = precision_score(np.asfarray(truth[['HorseRankTop50Percent']].values, float), np.asfarray(result[['HorseRankTop50Percent']].values, float))
+    r_score = recall_score(np.asfarray(truth[['HorseRankTop50Percent']].values, float),np.asfarray(result[['HorseRankTop50Percent']].values, float))
+    print("For Top50P: ")
+    print("p_score: ", p_score)
+    print("r_score: ", r_score)
+
+warnings.filterwarnings('ignore')
 training_df = pd.read_csv("training.csv")
 training_df['finish_time'] = training_df['finish_time'].apply(lambda frac: convert_to_float_time(frac))
 training_df = training_df.fillna(0)
@@ -110,7 +228,7 @@ testing_df = testing_df.merge(temp_df_trainer_index, left_on='trainer_index', ri
 testing_df['jockey_ave_rank'].fillna(7,inplace = True)
 testing_df['trainer_ave_rank'].fillna(7, inplace=True)
     
-lr_result,lr_running_time = lr_model(training_df, testing_df)
-
-print("lr_model: ")
-print("Running time : %s seconds" % lr_running_time)
+lr_result, lr_truth = lr_model(training_df, testing_df)
+nb_result, nb_truth = nb_model(training_df, testing_df)
+svm_result, svm_truth = svm_model(training_df, testing_df)
+rf_result, rf_truth = rf_model(training_df, testing_df)
--- preprocess.py
+++ preprocess.py
@@ -33,16 +33,19 @@ data['recent_ave_rank'] = data.recent_6_runs.apply(lambda x : ave(x))
 #2.2.3 Indexes
 #Horse.
 horse = np.array(data.horse_id.unique())
+print("horse number: ", horse.shape[0])
 def get_horse_index(string):
         return np.where(horse == string)[0][0]
 data['horse_index'] = data.horse_id.apply(lambda x: get_horse_index(x))
 #Jockey.
 jockey = np.array(data.jockey.unique())
+print("jockey number: ", jockey.shape[0])
 def get_jockey_index(string):
         return np.where(jockey == string)[0][0]
 data['jockey_index'] = data.jockey.apply(lambda x: get_jockey_index(x))
 #Trainer.
 trainer = np.array(data.trainer.unique())
+print("trainer number: ", trainer.shape[0])
 def get_trainer_index(string):
         return np.where(trainer == string)[0][0]
 data['trainer_index'] = data.trainer.apply(lambda x: get_trainer_index(x))
