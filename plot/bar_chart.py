995b53bf36219686bf7548c2fec9fea18fcbbdb9
VitoChan1 <vitochan1@hotmail.com>
2018-05-01 15:57:46 +0800

    regression and bar

---
+++ plot/bar_chart.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+
+def to_seconds(x):
+	x_arr = x.split('.')
+	return (int(x_arr[0]) * 60+int(x_arr[1]) + int(x_arr[2]) * 0.001)
+
+df_train = pd.read_csv('../training.csv',
+            header=None,
+            names=['finishing_position','horse_number','horse_name','horse_id','jockey','trainer','actual_weight',
+			'declared_horse_weight','draw','length_behind_winner','running_position_1','running_position_2','running_position_3','running_position_4',
+			'finish_time','win_odds','running_position_5','running_position_6','race_id',
+			'recent_6_runs','recent_ave_rank','jockey_ave_rank','trainer_ave_rank','race_distance'],
+                na_values=['---'])
+
+X_train_numeric = df_train[['actual_weight','declared_horse_weight','draw','win_odds','jockey_ave_rank','trainer_ave_rank','recent_ave_rank','race_distance']][0:].astype('float')
+
+
+y_train = df_train['finishing_position'][0:].astype('int')
+#print(y_train[:10])
+y_train[y_train!=1] = 0
+#print(y_train[:10])
+print(X_train_numeric.shape)
+print(y_train.shape)
+print(df_train.actual_weight[0])
+rf_model = RandomForestClassifier(max_depth=10, random_state=0)
+rf_model.fit(X_train_numeric, y_train)
+importatnce_list = rf_model.feature_importances_
+features_names = ['actual\n_weight','declared_\nhorse_weight','draw','win_odds','jockey_\nave_rank','trainer\n_ave_rank','recent_\nave_rank','race_\ndistance']
+sorted_features = [x for _, x in sorted(zip(importatnce_list,features_names), key=lambda pair: pair[0],reverse=True)]
+sorted_importance = sorted(importatnce_list,reverse=True)
+
+
+y_pos = np.arange(len(sorted_features))
+
+
+plt.bar(y_pos, sorted_importance, align='center', alpha=0.5)
+plt.xticks(y_pos, sorted_features,size=6,rotation='vertical')
+plt.ylabel('feature importance')
+plt.title('Feature Importance Bar Chart')
+
+plt.show()
--- regression.py
+++ regression.py
@@ -20,7 +20,7 @@ temp_j = test_raw_data[['jockey_index', 'finishing_position']]
 temp_j['jockey_ave_rank'] = temp_j.jockey_index.map(temp_j.groupby(['jockey_index']).finishing_position.mean())
 temp_j = temp_j.drop_duplicates(subset='jockey_index', keep='last')
 temp_j = temp_j.drop('finishing_position',axis = 1)
-test_raw_data = test_data.merge(temp_j, left_on='jockey_index', right_on='jockey_index', how='left')
+test_raw_data = test_raw_data.merge(temp_j, left_on='jockey_index', right_on='jockey_index', how='left')
 test_raw_data['jockey_ave_rank'].fillna(7,inplace=True)
 # Test: Average rank of trainer.
 temp_t = pd.DataFrame()
@@ -28,7 +28,7 @@ temp_t = test_raw_data[['trainer_index', 'finishing_position']]
 temp_t['trainer_ave_rank'] = temp_t.trainer_index.map(temp_t.groupby(['trainer_index']).finishing_position.mean())
 temp_t = temp_t.drop_duplicates(subset='trainer_index', keep='last')
 temp_t = temp_t.drop('finishing_position',axis = 1)
-test_raw_data = test_data.merge(temp_t, left_on='trainer_index', right_on='trainer_index', how='left')
+test_raw_data = test_raw_data.merge(temp_t, left_on='trainer_index', right_on='trainer_index', how='left')
 test_raw_data['trainer_ave_rank'].fillna(7,inplace=True)
 
 # Select features and convert dataframe to array
@@ -45,12 +45,12 @@ X_test = test_data[:,:8]
 y_test = test_data[:,8]
 
 # 4.2
-X_train_scaler = StandardScaler()
-y_train_scaler = StandardScaler()
-X_test_scaler = StandardScaler()
-X_train = train_scaler.fit_transform(X_train)
-y_train = train_scaler.fit_transform(y_train)
-X_test = test_scaler.fit_transform(X_test)
+#X_train_scaler = StandardScaler()
+#y_train_scaler = StandardScaler()
+#X_test_scaler = StandardScaler()
+#X_train = train_scaler.fit_transform(X_train)
+#y_train = train_scaler.fit_transform(y_train)
+#X_test = test_scaler.fit_transform(X_test)
 
 
 
@@ -63,40 +63,38 @@ def timeinseconds(timestring):
 y_train = map(lambda x: timeinseconds(x),y_train)
 y_test = map(lambda x: timeinseconds(x),y_test)
 # Max iteration
-mi = 5000
-print(X_train[:10],X_test[:10])
+mi = 3000
 
 # 4.1.1
-def svr_model(X, y):
+def svr_model(X, y, X_test, y_test):
+    print("SVR:")
     start_time = time.time()
     svr_model = SVR(kernel='rbf', C=1e3, gamma=0.1,max_iter=mi)
-    kernel_train = np.dot(X, X.T)
     rbf = svr_model.fit(X, y).score(X, y)
-    print(rbf,sig,pre)
+    #print(rbf)
+    #result = svr_model.predict(X_test)
+    #evaluation(result,y_test)
     # Run time
     running_time = time.time() - start_time
     print("Running time : %s seconds" % running_time)
 
 #4.1.2
-def gbrt_model(X, y):
+def gbrt_model(X, y, X_test, y_test):
+    print("GBRT:")
     start_time = time.time()
-    gbrt_model1 = GBRT(loss='ls')
-    gbrt_model2 = GBRT(loss='lad')
-    gbrt_model3 = GBRT(loss='huber')
-    gbrt_model4 = GBRT(loss='quantile')
-    ls = gbrt_model1.fit(X, y).score(X, y)
-    lad = gbrt_model1.fit(X, y).score(X, y)
-    huber = gbrt_model1.fit(X, y).score(X, y)
-    quantile = gbrt_model1.fit(X, y).score(X, y)
-    print(ls,lad,huber,quantile)
+    gbrt_model = GBRT(loss='huber')
+    huber = gbrt_model.fit(X, y).score(X, y)
+    #print(huber)
+    #result = gbrt_model.predict(X_test)
+    #evaluation(result,y_test)
     # Run time
     running_time = time.time() - start_time
     print("Running time : %s seconds" % running_time)
 
 # 4.2
 def evaluation(result,truth):
-    result = y_train_scaler.inverse_transform(result)
-    RMSE = sqrt(MSE(truth,result))
+    #result = y_train_scaler.inverse_transform(result)
+    RMSE = np.sqrt(MSE(truth,result))
     top1_sum = 0
     top3_sum = 0
     rank_sum = 0
@@ -117,7 +115,7 @@ def evaluation(result,truth):
             race_id = test_raw_data['race_id'][i]
             race_sum += 1
             top_id = i
-        else if (result[i] < result[top_id]):
+        elif (result[i] < result[top_id]):
                 top_id = i
         # Next sample
         i += 1
@@ -133,8 +131,5 @@ def evaluation(result,truth):
     print("Average_rank: ", rank_sum * 1.0/race_sum)
 
 # 4.2
-
-
-
-#svr_model(X,y)
-#gbrt_model(X,y)
+svr_model(X_train,y_train,X_test,y_test)
+gbrt_model(X_train,y_train,X_test,y_test)
